8b28da35877b4d35e0b81e8bde5ff14b341aaa7e
Intisy <wilderpizza99@gmail.com>
2025-03-16 18:05:28 +0100

    Added build arguments

--- src/main/java/io/github/intisy/gradle/inno/InnoSetupTask.java
+++ src/main/java/io/github/intisy/gradle/inno/InnoSetupTask.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class InnoSetupTask extends DefaultTask {
@@ -15,6 +16,8 @@ public class InnoSetupTask extends DefaultTask {
     String name;
     String icon;
     String version;
+    List<String> autoStartParameters;
+    List<String> parameters;
     Path jrePath;
     boolean autoStart;
     boolean debug;
@@ -34,6 +37,8 @@ public class InnoSetupTask extends DefaultTask {
                     innoSetup.setVersion(version);
                 innoSetup.setAutoStart(autoStart);
                 innoSetup.setJrePath(jrePath);
+                innoSetup.setParameters(parameters);
+                innoSetup.setAutoStartParameters(autoStartParameters);
                 innoSetup.buildInstaller();
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
